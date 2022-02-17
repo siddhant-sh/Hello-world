7066d6ffd975445280d50fd2772f0b0fd304cbcf
siddhant-sh <siddhantsh@cybage.com>
2022-02-17 13:15:25 +0530

    Calculator code added

--- src/main/java/com/hello/HelloWorld.java
+++ src/main/java/com/hello/HelloWorld.java
@@ -4,5 +4,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		Greeter greeter = new Greeter();
 		System.out.println(greeter.sayHello());
+		Calculator cal = new Calculator();
+		System.out.println(cal.sum(2, 2));
+		System.out.println(cal.divide(2, 2));
+		System.out.println(cal.minus(2, 2));
+		System.out.println(cal.multiply(2, 2));
 	}
 }
